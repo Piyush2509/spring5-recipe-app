fd9d2e8f9f8e5cb3b4691cfc36c6e1e3e1eae019
Piyush2509 <piyush.jayaswal@gmail.com>
2018-05-23 16:27:07 -0400

    Added new recipe form and controller method to save recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,9 +2,13 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,6 +30,20 @@ public class RecipeController {
 		log.debug("Getting recipe page");
 		model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 		return "recipe/show";
+	}
+	
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		log.debug("Getting new recipe form");
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+	
+	@PostMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		log.debug("Saving or updating recipe");
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);	
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 
 }
