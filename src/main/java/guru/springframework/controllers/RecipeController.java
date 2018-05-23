a90ba48eb0a47a7c58557736032866fa6251fad4
Piyush2509 <piyush.jayaswal@gmail.com>
2018-05-23 16:27:07 -0400

    Added ability to delete a recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,18 +17,46 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class RecipeController {
-	
+
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService) {
 		this.recipeService = recipeService;
 	}
-	
-	@RequestMapping("/recipe/show/{id}")
+
+	@GetMapping("/recipe/{id}/show")
 	public String ShowById(@PathVariable String id, Model model) {
-		log.debug("Getting recipe page");
+		log.debug("Getting recipe page for id: " + id);
 		model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
 		return "recipe/show";
+	}
+
+	@GetMapping("recipe/new")
+	public String newRecipe(Model model) {
+		log.debug("Getting new recipe form");
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
+
+	@GetMapping("/recipe/{id}/update")
+	public String updateRecipe(@PathVariable String id, Model model) {
+		log.debug("Getting recipe form for update for id: " + id);
+		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+		return "recipe/recipeform";
+	}
+
+	@PostMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		log.debug("Saving or updating recipe");
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+		return "redirect:/recipe/" + savedCommand.getId() + "/show";
+	}
+
+	@GetMapping("/recipe/{id}/delete")
+	public String deleteById(@PathVariable String id) {
+		log.debug("Deleting recipe: " + id);
+		recipeService.deleteById(Long.valueOf(id));
+		return "redirect:/";
 	}
 
 }
--- src/test/java/guru/springframework/services/RecipeServiceTest.java
+++ src/test/java/guru/springframework/services/RecipeServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.never;
 
@@ -17,6 +18,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.converters.RecipeCommandToRecipe;
 import guru.springframework.converters.RecipeToRecipeCommand;
 import guru.springframework.domain.Recipe;
@@ -25,9 +27,9 @@ import guru.springframework.repositories.RecipeRepository;
 /**
  * Created by piyush.b.kumar on May 21, 2018.
  */
-public class RecipeServiceImplTest {
+public class RecipeServiceTest {
 
-	RecipeServiceImpl recipeService;
+	RecipeService recipeService;
 
 	@Mock
 	RecipeRepository recipeRepository;
@@ -71,5 +73,33 @@ public class RecipeServiceImplTest {
 		verify(recipeRepository, times(1)).findById(anyLong());
 		verify(recipeRepository, never()).findAll();
 	}
+
+    @Test
+    public void findCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void deleteByIdTest() throws Exception {
+    	Long idToDelete = 2L;
+    	recipeService.deleteById(idToDelete);
+    	
+    	verify(recipeRepository, times(1)).deleteById(anyLong());
+    }
 
 }
