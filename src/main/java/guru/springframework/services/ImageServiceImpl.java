acb10f8cf1a0586372bbdb2602c8c943d39939a2
Piyush2509 <piyush.jayaswal@gmail.com>
2018-05-24 22:17:17 -0400

    Added ability to save uploaded recipe image

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -1,8 +1,12 @@
 package guru.springframework.services;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -12,9 +16,29 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+	private final RecipeRepository recipeRepository;
+
+	public ImageServiceImpl(RecipeRepository recipeRepository) {
+		this.recipeRepository = recipeRepository;
+	}
+
 	@Override
 	public void saveRecipeImage(Long recipeId, MultipartFile imageFile) {
-		log.debug("Received a file");
+		try {
+			Recipe recipe = recipeRepository.findById(recipeId).get();
+			Byte[] byteObjects = new Byte[imageFile.getBytes().length];
+
+			int i = 0;
+			for (byte b : imageFile.getBytes()) {
+				byteObjects[i++] = b;
+			}
+
+			recipe.setImage(byteObjects);
+			recipeRepository.save(recipe);
+		} catch (IOException e) {
+			log.error("Error occured", e);
+			e.printStackTrace();
+		}
 	}
 
 }
